b02581939f009c3343d8e15ab409f9fc874feb8f
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-13 23:04:48 +0300

    Initial commit

---
+++ src/test/java/com/microshop/stockmanagement/chartservice/ChartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microshop.stockmanagement.chartservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
