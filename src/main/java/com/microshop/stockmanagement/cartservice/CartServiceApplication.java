43d72f9c9982fd753d10dae9ec8495b4e4709cf0
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-14 15:45:38 +0300

    Added feignclient and eureka annotations

--- src/main/java/com/microshop/stockmanagement/cartservice/CartServiceApplication.java
+++ src/main/java/com/microshop/stockmanagement/cartservice/CartServiceApplication.java
@@ -2,8 +2,13 @@ package com.microshop.stockmanagement.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
