913424210f8ad20640ca9875a627147a2df6cefe
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-14 11:28:24 +0300

    Added response classes.

---
+++ src/main/java/com/microshop/stockmanagement/cartservice/response/FriendlyMessage.java
@@ -0,0 +1,13 @@
+package com.microshop.stockmanagement.cartservice.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class FriendlyMessage {
+
+    private String title; // error succes gibi mesajları içerir.
+    private String description; // record succesfully created gibi hata veya başarılı kayıtların açıklamasını içerir
+    private String buttonPositive; //UI tarafında butonun nerede bulunacğaını belirtir.
+}
---
+++ src/main/java/com/microshop/stockmanagement/cartservice/response/UserResponse.java
@@ -0,0 +1,22 @@
+package com.microshop.stockmanagement.cartservice.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class UserResponse {
+
+    private long userId;
+    private String userName;
+    private String userSurname;
+    private String userPassword;
+    private String userEmail;
+    private String userAddress;
+    private String userPhoneNumber;
+    private Long userCreatedDate;
+    private Long userUpdatedDate;
+}
