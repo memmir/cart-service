cf8ea3d9085e22e5149d6b074a2d7ebd3e1194bf
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-14 15:46:13 +0300

    Added lombok annotaitons to order entity.

--- src/main/java/com/microshop/stockmanagement/cartservice/entity/Order.java
+++ src/main/java/com/microshop/stockmanagement/cartservice/entity/Order.java
@@ -1,12 +1,20 @@
 package com.microshop.stockmanagement.cartservice.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name= "order")
 public class Order {
 
