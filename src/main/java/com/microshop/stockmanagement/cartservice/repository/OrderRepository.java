0367bc1717634bc5215d59d3de37e6c39165fb2e
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-05-14 11:08:12 +0300

    Created item and order repository interfaces.

---
+++ src/main/java/com/microshop/stockmanagement/cartservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.microshop.stockmanagement.cartservice.repository;
+
+import com.microshop.stockmanagement.cartservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
